1a5586ff77322ec23f301db2fcfcab2ffce98695
Roberto Carlos <74996793+Roberto-D01@users.noreply.github.com>
2024-01-25 21:10:24 -0300

    vetores

--- src/application/ProgramVet.java
+++ src/application/ProgramVet.java
@@ -1,7 +1,19 @@
 package application;
 
+import entities.Rent;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ProgramVet {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many rooms will be rented? ");
+        Rent r = new Rent("Maria","maria@gmail.com");
+
+        sc.close();
     }
 }
